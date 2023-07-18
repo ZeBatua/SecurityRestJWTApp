a6efe1fc06b8e9e37cee2f9c1c797602d0e7164c
thalassa_home <blinov.gosha@mail.ru>
2023-07-18 21:56:17 +0300

    Initial commit

---
+++ src/main/java/rest/app/SecurityRestJwtAppApplication.java
@@ -0,0 +1,13 @@
+package rest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityRestJwtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityRestJwtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rest/app/SecurityRestJwtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package rest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityRestJwtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
